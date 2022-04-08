1499fde2903d9a0938fbf4d60e5d5cfede37b337
emisaar <emisaar0406@users.noreply.github.com>
2022-04-07 20:44:11 -0500

    Ejercicio 15

--- actividad2_3.rkt
+++ actividad2_3.rkt
@@ -169,6 +169,19 @@
 ; #t
 
 ; ---- Ejercicio 15 ----
+(define (linear-search lst x check)
+  (cond
+  [(null? lst) #f]
+  [(if(check x (first lst)) 
+    0
+    (+ 1 (linear-search (rest lst) x check)))]))
+
+(display "\nEjercicio 15 - linear-search\n")
+(linear-search '() 5 =)
+(linear-search '(48 77 30 31 5 20 91 92 69 97 28 32 17 18 96) 5 =)
+(linear-search '("red" "blue" "green" "black" "white") "black" string=?)
+(linear-search '(a b c d e f g h) 'h equal?)
+
 ; ---- Ejercicio 18 ---- Func
 (define (integral a b n f)
 
